97b7040d3bc1a0071cf330d8f645dc1d406b6487
Drew Dennison <dennison@mit.edu>
2017-12-14 13:30:26 -0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-eval("function() { console.log(2 + 'pixel')}")
+eval("function() { console.log(2 + 'lol')}")
 
 ReactDOM.render(
   <App />,
